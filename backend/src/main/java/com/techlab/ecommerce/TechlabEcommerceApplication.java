eff67a6644648567a7b826f6a517a43936828a18
JTejad <jtejada@fi.uba.ar>
2026-07-06 19:45:25 -0300

    Agrego backend Spring Boot base

---
+++ backend/src/main/java/com/techlab/ecommerce/TechlabEcommerceApplication.java
@@ -0,0 +1,11 @@
+package com.techlab.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechlabEcommerceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TechlabEcommerceApplication.class, args);
+    }
+}
